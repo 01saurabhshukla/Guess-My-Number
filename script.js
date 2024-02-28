516cd413c8e90da5825a6be11e0e1ac6d15bed78
Saurabh Shukla <103saurabhshukla@gmail.com>
2024-02-28 15:20:05 +0530

    fixs

--- script.js
+++ script.js
@@ -4,12 +4,11 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore = 0;
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const inputNumber = Number(document.querySelector('.guess').value);
-  // console.log(inputNumber);
+
 
   if (!inputNumber) {
     document.querySelector('.message').textContent = '✖ No Number Entered';
@@ -18,15 +17,6 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    // let Hscore = Number(document.querySelector('.score').innerText);
-
-    // if (Hscore > highscore) {
-    //     document.querySelector('#highscore').textContent = Hscore;
-    // }
-    // if (score > highscore) {
-    //     highscore = score;
-    //     document.querySelector('#highscore').textContent = highscore;
-    //   }
   
   } else if (inputNumber !== secretNumber) {
     if (score > 1) {
