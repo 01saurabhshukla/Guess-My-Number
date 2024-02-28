9f05912f870c703a4ef9e6384f9a3983d1ce7e95
Saurabh Shukla <103saurabhshukla@gmail.com>
2024-02-28 14:27:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// If you want your code to be the most efficient and effective code, then when you are trying to retrieve an id then you should use getElementById. If you are trying to retrieve a class or tag then you should use querySelector.
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const inputNumber = Number(document.querySelector('.guess').value);
+  // console.log(inputNumber);
+
+  if (!inputNumber) {
+    document.querySelector('.message').textContent = '✖ No Number Entered';
+  } else if (inputNumber === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.getElementById('body').style.backgroundColor = '#60b347';
+    document.getElementById('.body').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (inputNumber !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.score').textContent =
+        inputNumber > secretNumber ? 'Too High ' : 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('number').style.width = '15rem';
+});
